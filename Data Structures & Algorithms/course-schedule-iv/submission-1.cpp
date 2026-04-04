311374f02cd2eb5dd1402329b6fc73aceec06091
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-04 09:23:50 +0530

    Add: course-schedule-iv - submission-1

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<bool>> adjm(n, vector<bool>(n, false));
+        for(auto &p: prerequisites) {
+            adjm[p[0]][p[1]] = true;
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                for(int k=0; k<n; k++) {
+                    if(!adjm[i][j]) adjm[i][j] = adjm[i][k] & adjm[k][j];
+                }
+            }
+        }
+        vector<bool> res;
+        for(auto &q: queries) {
+            res.push_back(adjm[q[0]][q[1]]);
+        }
+        return res;
+    } 
+};
