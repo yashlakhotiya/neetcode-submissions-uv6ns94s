a9181da47577cb695c43bb61f91ef32c4f7fc056
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-11 15:49:58 +0530

    Add: greatest-common-divisor-traversal - submission-3

---
+++ Data Structures & Algorithms/greatest-common-divisor-traversal/submission-3.cpp
@@ -0,0 +1,55 @@
+class DS {
+    vector<int> parent, size;
+    int nocomponents;
+    public:
+    DS(int n) : nocomponents(n) {
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        size.assign(n,1);
+    }
+
+    int find(int i) {
+        if(i == parent[i]) return i;
+        return parent[i]=find(parent[i]);
+    }
+
+    bool unn(int i, int j) {
+        int p1 = find(i), p2 = find(j);
+        if(p1==p2) return false;
+        if(size[p1]<size[p2]) swap(p1, p2);
+        parent[p2] = p1;
+        size[p1] += size[p2];
+        nocomponents--;
+        return true;
+    }
+
+    int getnocomponents() {
+        return nocomponents;
+    }
+};
+
+class Solution {
+public:
+    bool canTraverseAllPairs(vector<int>& nums) {
+        int n = nums.size();
+        DS ds(n);
+        unordered_map<int,int> adj;
+        for(int i=0; i<n; i++) {
+            int num = nums[i];
+            for(int factor=2; factor*factor<=num; factor++) {
+                if(num%factor == 0) {
+                    if(adj.count(factor)) ds.unn(adj[factor], i);
+                    else adj[factor] = i;
+                    while(num%factor == 0) {
+                        num /= factor;
+                    }
+                }
+            }
+            if(num>1) {
+                if(adj.count(num)) ds.unn(adj[num], i);
+                else adj[num] = i;
+            }
+        }
+        return ds.getnocomponents()==1;
+    }
+};
