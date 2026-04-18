df54e561470fbce63dc8b9c6a2d136f7c20c251a
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-18 07:21:37 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countdecodings(string &s, int i, vector<int> &dp) {
+        if(s[i]=='0') return 0;
+        if(i >= s.size()) return 1;
+        if(dp[i] != -1) return dp[i];
+        string enc1 = s.substr(i, 1);
+        string enc2 = "";
+        if(i+1 < s.size()) {
+            enc2 = s.substr(i, 2);
+            if(stoi(enc2) > 26) enc2 = "";
+        }
+        int cnt = countdecodings(s, i+1, dp);
+        if(enc2 != "") cnt += countdecodings(s, i+2, dp);
+        return dp[i]=cnt;
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1);
+        return countdecodings(s, 0, dp);
+    }
+};
