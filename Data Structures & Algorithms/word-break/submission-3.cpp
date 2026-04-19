e36d9ffa35d3808f8da69e20581640427d804010
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-19 18:37:56 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool findwords(string &s, unordered_set<string>& wordmp, int i, vector<int> &dp) {
+        if(i >= s.size()) return true;
+        if(dp[i]!=-1) return dp[i]?true:false;
+        for(int j=i; j<s.size(); j++) {
+            int L = j-i+1;
+            string word = s.substr(i, L);
+            if(wordmp.count(word)) {
+                if(findwords(s, wordmp, j+1, dp)) return dp[i]=true;
+            }
+        }
+        return dp[i]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordmp(wordDict.begin(), wordDict.end());
+        vector<int> dp(s.size()+1, false);
+        dp[s.size()] = true;
+        for(int i=s.size()-1; i>=0; i--) {
+            for(int j=i; j<s.size(); j++) {
+                int L = j-i+1;
+                string word = s.substr(i, L);
+                if(wordmp.count(word)) {
+                    if(dp[j+1]) dp[i]=true;
+                }
+            }
+            // dp[i]=false;
+        }
+        return dp[0];
+    }
+};
