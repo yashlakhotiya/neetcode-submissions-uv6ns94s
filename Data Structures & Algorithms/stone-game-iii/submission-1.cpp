88aa2a0b2be11e161972eaec95cfa5d2273d3011
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-22 06:51:15 +0530

    Add: stone-game-iii - submission-1

---
+++ Data Structures & Algorithms/stone-game-iii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dfs(vector<int>& stoneValue, int i, vector<int> &dp) {
+        int n = stoneValue.size();
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int res = stoneValue[i] - dfs(stoneValue, i+1,dp);
+        if(i+1 < n)
+            res = max(res, stoneValue[i] + stoneValue[i+1] - dfs(stoneValue, i+2,dp));
+        if(i+2 < n)
+            res = max(res, stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dfs(stoneValue, i+3,dp));
+
+        return dp[i]=res;
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size()+1, -1);
+        int res = dfs(stoneValue, 0,dp);
+        if(res == 0) return "Tie";
+        else if(res < 0) return "Bob";
+        else return "Alice";
+    }
+};
