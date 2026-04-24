7c2d9ba2b3111a9270752ac3d62a3464e480ad7b
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-24 07:21:02 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int nextsell = 0, nextbuy = 0, lastbuy = 0, curbuy = 0, cursell = 0;
+        for(int i=prices.size()-1; i>=0; i--) {
+            for(int canbuy=1; canbuy>=0; canbuy--) {
+                // int res = 0;
+                if(canbuy) {
+                    curbuy = max(-prices[i]+nextsell, nextbuy);
+                } else {
+                    cursell = max(prices[i]+lastbuy, nextsell);
+                }
+                // curr[canbuy] = res;
+            }
+            lastbuy = nextbuy;
+            nextbuy = curbuy;
+            nextsell = cursell;
+        }
+        return nextbuy;
+    }
+};
+
