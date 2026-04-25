8ff02a7bfd2de8b2412a1d265f85a854a73b74e1
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-25 09:00:28 +0530

    Add: target-sum - submission-7

---
+++ Data Structures & Algorithms/target-sum/submission-7.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int dfs(vector<int> &nums, int i, int cursum, int subsettarget) {
+        if(i >= nums.size()) return subsettarget==cursum;
+        return dfs(nums, i+1, cursum+nums[i], subsettarget)+dfs(nums,i+1, cursum, subsettarget);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalsum = accumulate(nums.begin(), nums.end(), 0);
+        if((target+totalsum)%2 != 0) return 0;
+        int subsettarget = (target+totalsum)/2;
+        return dfs(nums, 0, 0, subsettarget);
+    }
+};
