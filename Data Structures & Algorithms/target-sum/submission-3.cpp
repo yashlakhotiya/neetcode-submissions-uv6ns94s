33fb98c4c65585348caa1fbbe3b94cb834e68af3
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-25 08:41:37 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dfs(vector<int> &nums, int i, int cursum, int target, vector<vector<int>> &dp, int totalsum) {
+        if(i >= nums.size()) return cursum==target;
+        if(dp[i][cursum+totalsum] != 1e9) return dp[i][cursum+totalsum];
+        return dp[i][cursum+totalsum] = dfs(nums, i+1, cursum+nums[i], target, dp, totalsum) + dfs(nums, i+1, cursum-nums[i], target, dp, totalsum);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalsum = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> dp(nums.size()+1, vector<int>(2*totalsum+1,  0));
+        for(int i=nums.size(); i>=0; i--) {
+            for(int cursum=totalsum; cursum >=-totalsum; cursum--) {
+                if(i >= nums.size()) dp[i][cursum+totalsum] = cursum==target;
+                // if(dp[i][cursum+totalsum] != 1e9) return dp[i][cursum+totalsum];
+                else dp[i][cursum+totalsum] = dp[i+1][cursum+nums[i]+totalsum]+ dp[i+1][cursum-nums[i]+totalsum];
+            }
+        }
+        return dp[0][0+totalsum];
+    }
+};
+
