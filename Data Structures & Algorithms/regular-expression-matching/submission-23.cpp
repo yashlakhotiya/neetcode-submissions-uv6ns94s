abb5fd21a5d296b7f817c7082ccad7a839424b6e
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-28 07:55:47 +0530

    Add: regular-expression-matching - submission-23

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-23.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool ismatch(string &s, string &p, int sind, int pind, vector<vector<int>> &dp) {
+        if(pind >= p.size()) {
+            return sind >= s.size();
+        }
+        if(dp[sind][pind] != -1) return dp[sind][pind];   
+        bool res = false;
+        if(pind+1 < p.size() && p[pind+1] == '*') {
+            if(sind >= s.size()) res = ismatch(s, p, sind, pind+2, dp);
+            else res = ismatch(s, p, sind, pind+2,dp) || 
+            ((p[pind] == '.' || s[sind]==p[pind]) && ismatch(s, p, sind+1, pind,dp));
+        } else {
+            if(sind >= s.size()) res = false;
+            else res = dp[sind][pind] = (p[pind] == '.' || s[sind]==p[pind]) && ismatch(s, p, sind+1, pind+1,dp);
+        }
+        return dp[sind][pind] = res;
+    }
+    bool isMatch(string s, string p) {
+        // vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
+        vector<int> curr(p.size()+1, 0), next(p.size()+1, 0);
+        for(int sind=s.size(); sind>=0; sind--) {
+            for(int pind=p.size(); pind>=0; pind--) {
+                bool res = false;
+                if(pind >= p.size()) {
+                    res = (sind >= s.size());
+                }
+                // else if(dp[sind][pind] != -1) dp[sind][pind] = dp[sind][pind];   
+                else {
+                    if(pind+1 < p.size() && p[pind+1] == '*') {
+                        if(sind >= s.size()) res = curr[pind+2];
+                        else res = curr[pind+2] || 
+                        ((p[pind] == '.' || s[sind]==p[pind]) && next[pind]);
+                    } else {
+                        if(sind >= s.size()) res = false;
+                        else res = (p[pind] == '.' || s[sind]==p[pind]) && next[pind+1];
+                    }
+                }
+                curr[pind] = res;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+};
