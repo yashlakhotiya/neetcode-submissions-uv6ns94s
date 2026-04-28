2cf68f5bd1ae86be85f98c5647cc946321860c4b
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-28 06:32:10 +0530

    Add: regular-expression-matching - submission-15

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-15.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool ismatch(string &s, string &p, int sind, int pind, vector<vector<int>> &dp) {
+        if(pind >= p.size()) {
+            return sind >= s.size();
+        }
+        if(dp[sind][pind] != -1) return dp[sind][pind];   
+        if(pind+1 < p.size() && p[pind+1] == '*') {
+            if(sind >= s.size()) return dp[sind][pind] = ismatch(s, p, sind, pind+2, dp);
+            return dp[sind][pind] = ismatch(s, p, sind, pind+2,dp) || ((p[pind] == '.' || s[sind]==p[pind]) && (ismatch(s, p, sind+1, pind,dp) || ismatch(s, p, sind+1, pind+2,dp)));
+        } else {
+            if(sind >= s.size()) return false;
+            else return dp[sind][pind] = (p[pind] == '.' || s[sind]==p[pind]) && ismatch(s, p, sind+1, pind+1,dp);
+        }
+    }
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
+        return ismatch(s, p, 0, 0, dp);
+    }
+};
