f83658305a382d0dd5fee09fbd2e331218b6b852
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-29 09:57:14 +0530

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int targetidx = nums.size()-1;
+        for(int i=nums.size()-2; i>=0; i--) {
+            if(i+nums[i] >= targetidx) targetidx = i;
+        }
+        return targetidx==0;
+    }
+};
