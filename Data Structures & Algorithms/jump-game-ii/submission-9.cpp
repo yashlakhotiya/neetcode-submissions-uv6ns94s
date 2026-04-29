7652dd3699a2a206b70d81ebb84e39802b656e6b
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-29 10:41:31 +0530

    Add: jump-game-ii - submission-9

---
+++ Data Structures & Algorithms/jump-game-ii/submission-9.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l=0, r=0, res=0;
+        while(r < nums.size()-1) {
+            int farthest = 0;
+            for(int i=l; i<=r; i++) {
+                farthest = max(farthest, i+nums[i]);
+            }
+            l = r+1, r = farthest;
+            res++;
+        }
+        return res;
+    }
+};
