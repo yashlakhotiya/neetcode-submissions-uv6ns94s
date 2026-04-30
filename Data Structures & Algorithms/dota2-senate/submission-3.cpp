8291e99a4308ec4e14f961e569e3cb49c9e7c369
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-30 08:48:59 +0530

    Add: dota2-senate - submission-3

---
+++ Data Structures & Algorithms/dota2-senate/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findnextoppavailablesenate(string &senate, vector<bool> &rightremoved, int i) {
+        int n = senate.size();
+        for(int k=i+1; k<i+n; k++) {
+            if(senate[k%n] != senate[i] && !rightremoved[k%n]) return k%n;
+        }
+        return -1;
+    }
+    string getSenateName(string &senate, int i) {
+        return senate[i]=='R'?"Radiant":"Dire";
+    }
+    string predictPartyVictory(string senate) {
+        vector<bool> rightremoved(senate.size(), false);
+        int dcount=0, rcount=0;
+        for(char c: senate) {
+            if(c == 'R') rcount++;
+            else dcount++;
+        }
+        int i = 0, n = senate.size();
+        while(true) {
+            if(rightremoved[i]) {
+                i = (i+1)%n;
+                continue;
+            }
+            int nextoppositesenateidx = findnextoppavailablesenate(senate, rightremoved, i);
+            if(nextoppositesenateidx == -1) return getSenateName(senate, i);
+            else {
+                rightremoved[nextoppositesenateidx] = true;
+            }
+            i = (i+1)%n;
+        }
+        return "";
+    }
+};
