f8e7d74c57b210921dd8504625816845bbe4663a
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-04-30 10:29:12 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> lastindex(26, 0);
+        for(int i=0; i<s.size(); i++) {
+            lastindex[s[i]-'a'] = i;
+        }
+        vector<int> res;
+        int currentlast = -1, l = 0;
+        for(int i=0; i<s.size(); i++) {
+            int last = lastindex[s[i]-'a'];
+            currentlast = max(last, currentlast);
+            if(i == currentlast) {
+                res.push_back(i-l+1);
+                l=i+1;
+            }
+        }
+        return res;
+    }
+};
