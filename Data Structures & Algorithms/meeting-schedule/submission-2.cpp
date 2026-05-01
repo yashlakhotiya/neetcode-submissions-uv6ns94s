a7026786813f50cbe6ed94324b371304a26ee55d
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-05-01 08:19:32 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            return a.start<b.start;
+        });
+        for(int i=1; i<intervals.size(); i++) {
+            int ps = intervals[i-1].start, pe = intervals[i-1].end;
+            int cs = intervals[i].start, ce = intervals[i].end;
+            if(cs < pe) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
