d92d2c62661c2007491f741d9de3ba64ace3b2d2
Prakirti Lakhotiya <39040721+yashlakhotiya@users.noreply.github.com>
2026-06-12 07:14:13 +0530

    Add: cheapest-flight-path - submission-25

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-25.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        if(src == dst) return 0;
+        vector<vector<int>> adj[n];
+        for(auto &f: flights) {
+            adj[f[0]].push_back({f[1], f[2]});
+        }
+        set<vector<int>> s;
+        s.insert({0,-1,src});
+        while(!s.empty()) {
+            auto v = *s.begin();
+            s.erase(s.begin());
+            if(v[2] == dst) {
+                return v[0];
+            }
+            if(v[1]>=k) continue;
+            for(auto neigh: adj[v[2]]) {
+                s.insert({v[0]+neigh[1],v[1]+1,neigh[0]});
+            }
+        }
+        return -1;
+    }
+};
